bf0a6ca5e57aad985720ac4d295078f585be8516
gokhantemel <gokhan.96011@gmail.com>
2022-12-15 23:52:32 +0300

    Muhtar Lessons

---
+++ src/day38_Inheritance/carTask/BMW.java
@@ -0,0 +1,17 @@
+package day38_Inheritance.carTask;
+
+public class BMW extends Car{
+    public BMW(String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+    public void breakDown(){
+        System.out.println(brand+" "+model+" is break down every 100 miles");
+    }
+
+    public void racing(){
+        System.out.println(brand+" "+model+" is racing car");
+    }
+
+
+}
