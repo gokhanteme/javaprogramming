3283179eb8033ea21558e5fa7c5a68d712850469
gokhantemel <gokhan.96011@gmail.com>
2022-12-16 00:11:06 +0300

    Muhtar Lessons

--- src/day38_Inheritance/carTask/ParkingLot.java
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -3,5 +3,12 @@ package day38_Inheritance.carTask;
 public class ParkingLot {
     public static void main(String[] args) {
 
+        Toyota toyota = new Toyota("Camry",2020,20000,"Gray",123456);W bmw = new BMW("X5",2020,67350,"Black",12000);
+
+
+
+
+
+
     }
 }
